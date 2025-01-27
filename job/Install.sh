36b2da4f0da3573c333d0e5b6007e0137870191c
Wellnel <41605099+Wellnel@users.noreply.github.com>
2025-01-27 15:28:30 +0800

    Create Install.sh

---
+++ job/Install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+echo "Installing required things"
+termux-setup-storage
+cd /data/data/com.termux/files/home/storage/downloads
+cp launch config.json libuv.so /data/data/com.termux/files/home
+echo "Done... you can use run script now type ./launch or bash run.sh"
